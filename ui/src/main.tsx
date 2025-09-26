1609fb5b20101e65f4c3abe0f955be8ac7241767
LeanderWe <ddlkgek0182@hotmail.com>
2025-09-26 21:37:00 +0800

    update front

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,25 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { WagmiProvider } from 'wagmi'
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-import { config } from './wagmi'
-import App from './App.tsx'
-
-import '@rainbow-me/rainbowkit/styles.css'
 import './index.css'
-
-const queryClient = new QueryClient()
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          <App />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <App />
   </StrictMode>,
 )
